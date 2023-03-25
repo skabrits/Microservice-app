0cfac023686614f4933a30bc46705d64f09289ed
skabrits <seva.kabrits@gmail.com>
2023-03-26 02:11:30 +0300

    finish

---
+++ backend/src/registration/app/registration.Logic/user.py
@@ -0,0 +1,25 @@
+import random
+import pymongo
+import os
+
+def register(login, password):
+    user_id = generate_id()
+    user = { "login": login, "password": password, "_id": user_id }
+    users_table = connect_to_mongodb("users")
+    users_table.insert_one(user)
+    return user_id
+
+def generate_id():
+    random.seed(str(login+password))
+    seed_part = random.randint(100000,1000000)
+    random.seed()
+    random_part = random.randint(100000,1000000)
+    return str(seed_part)+str(random_part)
+
+def connect_to_mongodb(table_name):
+    connection_url = os.getenv('DB_URL')
+    myclient = pymongo.MongoClient(connection_url)
+    mydb = myclient["project"]
+    return mydb[table_name]
+
+    
