57f9d2a78db7cecc3e0771cdaaf6a1e873da9183
skabrits <seva.kabrits@gmail.com>
2023-05-15 01:01:10 +0300

    test method

--- backend/src/registration/app/app/__init__.py
+++ backend/src/registration/app/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
 
@@ -8,5 +9,8 @@ CORS(app)
 UPLOAD_FOLDER = '/app/files'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 from app import views
